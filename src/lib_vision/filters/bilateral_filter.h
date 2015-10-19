1177c3373475cd55ac1c4820a9de6b1cb65c7a4c
Pierluc Bedard <blindmiror@gmail.com>
2015-10-19 12:59:53 -0400

    Formatting file according to convetions

--- src/lib_vision/filters/bilateral_filter.h
+++ src/lib_vision/filters/bilateral_filter.h
@@ -1,51 +1,59 @@
-#ifndef VISION_FILTER_BILATERALFILTER_H_
-#define VISION_FILTER_BILATERALFILTER_H_
+/**
+ * \file	bilateral_filter.h
+ * \author  Jérémie St-Jules Prévôt <jeremie.st.jules.prevost@gmail.com>
+ * \date	14/12/2014
+ * \copyright	Copyright (c) 2015 SONIA AUV ETS. All rights reserved.
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
 
-//==============================================================================
-// I N C L U D E   F I L E S
+#ifndef LIB_VISION_FILTERS_BILATERAL_FILTER_H_
+#define LIB_VISION_FILTERS_BILATERAL_FILTER_H_
 
 #include <lib_vision/filter.h>
-//#include <opencv2/imgproc/imgproc.hpp>
 
 namespace vision_filter {
 
-//==============================================================================
-// C L A S S E S
+class BilateralFilter : public Filter {
+ public:
+  //============================================================================
+  // P U B L I C   C / D T O R
 
-    class BilateralFilter : public Filter {
-    public:
-      //============================================================================
-      // C O N S T R U C T O R S   A N D   D E S T R U C T O R
+  explicit BilateralFilter(const GlobalParamHandler &globalParams)
+      : Filter(globalParams),
+        _enable("Enable", false, parameters_),
+        _diameter("Diameter", -100, 0, 100, parameters_),
+        _sigma_color("Sigm_color", 0, 0, 300, parameters_),
+        _sigma_space("Sigma_space", 0, 0, 300, parameters_) {
+    setName("BilateralFilter");
+  }
 
-      explicit BilateralFilter(const GlobalParamHandler &globalParams)
-              : Filter(globalParams),
-                _enable("Enable", false, parameters_),
-                _diameter("Diameter", -100, 0, 100, parameters_),
-                _sigma_color("Sigm_color", 0, 0, 300, parameters_),
-                _sigma_space("Sigma_space", 0, 0, 300, parameters_) {
-        setName("BilateralFilter");
-      }
+  virtual ~BilateralFilter() {}
 
-      virtual ~BilateralFilter() {}
+  //============================================================================
+  // P U B L I C   M E T H O D S
 
-      //============================================================================
-      // P U B L I C   M E T H O D S
+  virtual void execute(cv::Mat &image) {
+    if (_enable()) {
+      cv::Mat blurred;
+      cv::bilateralFilter(image, blurred, _diameter(), _sigma_color(),
+                          _sigma_space());
 
-      virtual void execute(cv::Mat &image) {
-        if (_enable()) {
-          cv::Mat blurred;
-          cv::bilateralFilter(image, blurred, _diameter(), _sigma_color(), _sigma_space() );
+      blurred.copyTo(image);
+    }
+  }
 
-          blurred.copyTo(image);
-        }
-      }
+ private:
+  // Params
+  BooleanParameter _enable;
 
-    private:
-      // Params
-      BooleanParameter _enable;
-      IntegerParameter _diameter, _sigma_color, _sigma_space;
-    };
+  IntegerParameter _diameter;
+
+    IntegerParameter _sigma_color;
+
+    IntegerParameter _sigma_space;
+};
 
 }  // namespace vision_filter
 
-#endif  // VISION_FILTER_INRANGE_H_
+#endif  // LIB_VISION_FILTERS_BILATERAL_FILTER_H_
