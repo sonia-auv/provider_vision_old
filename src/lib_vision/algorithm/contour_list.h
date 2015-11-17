8b25f2208f8295e215448838d0e9b8db53d90e39
Thibaut Mattio <thibaut.mattio@gmail.com>
2015-11-17 15:19:05 -0500

    Adding the full license in all files

--- src/lib_vision/algorithm/contour_list.h
+++ src/lib_vision/algorithm/contour_list.h
@@ -1,8 +1,26 @@
-/*
- * contour_finder.h
+/**
+ * \file	contour_list.h
+ * \author	Jérémie St-Jules Prévôt <jeremie.st.jules.prevost@gmail.com>
+ * \author  Pierluc Bédard <pierlucbed@gmail.com>
  *
- *  Created on: Aug 24, 2015
- *      Author: jeremie
+ * \copyright Copyright (c) 2015 S.O.N.I.A. All rights reserved.
+ *
+ * \section LICENSE
+ *
+ * This file is part of S.O.N.I.A. software.
+ *
+ * S.O.N.I.A. software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * S.O.N.I.A. software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with S.O.N.I.A. software. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef LIB_VISION_SRC_LIB_VISION_ALGORITHM_CONTOUR_LIST_H_
