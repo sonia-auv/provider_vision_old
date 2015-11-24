674ac5c8f4f1a1ba7278dd8f2fa5a308af9eab14
Thibaut Mattio <thibaut.mattio@gmail.com>
2015-11-23 19:15:45 -0500

    Base unit test for detection tasks

--- test/detection_task_manager_test.cc
+++ test/detection_task_manager_test.cc
@@ -8,10 +8,26 @@
  * found in the LICENSE file.
  */
 
+#include <iostream>
 #include <gtest/gtest.h>
 #include <opencv2/opencv.hpp>
 #include "provider_vision/utils/config.h"
+#include "provider_vision/server/detection_task_manager.h"
 #include "provider_vision/server/media_manager.h"
+
+auto mmgr = provider_vision::MediaManager();
+
+std::stringstream ss;
+ss << provider_vision::kProjectPath << "test/test_image.png";
+auto streamer = mmgr.StartStreamingMedia(ss.str());
+
+TEST(DetectionTaskManager, start_detection) {
+  auto dmgr = provider_vision::DetectionTaskManager();
+  dmgr.StartDetectionTask(streamer, "image_feed", "test");
+  ASSERT_TRUE(dmgr.GetAllDetectionTasksName().size() == 1);
+  ASSERT_TRUE(*(dmgr.GetAllDetectionTasksName().begin()) == "test");
+  ASSERT_TRUE();
+}
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
