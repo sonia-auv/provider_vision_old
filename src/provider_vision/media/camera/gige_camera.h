230f595f0e84f12feef315a6cb55dd1d1d1e5038
Antoine Dozois <dozois.a@gmail.com>
2016-05-04 12:51:11 -0400

    WIP: created gige_camera.h

---
+++ src/provider_vision/media/camera/gige_camera.h
@@ -0,0 +1,22 @@
+//
+// Created by sonia on 5/4/16.
+//
+
+#ifndef PROVIDER_VISION_GIGE_CAMERA_H
+#define PROVIDER_VISION_GIGE_CAMERA_H
+
+//#include "GenApi/GenApi.h"
+//#include "gevapi.h"
+#include <lib_atlas/sys/timer.h>
+#include <sensor_msgs/image_encodings.h>
+#include <memory>
+#include <mutex>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include "provider_vision/media/camera/base_camera.h"
+#include "provider_vision/media/camera/base_media.h"
+#include "provider_vision/media/context/base_context.h"
+
+
+
+#endif //PROVIDER_VISION_GIGE_CAMERA_H
