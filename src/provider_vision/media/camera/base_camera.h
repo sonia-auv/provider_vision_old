fb08a81b76f3dae03eb4cc171db1c6624b21cec5
Thibaut Mattio <thibaut.mattio@gmail.com>
2016-05-23 11:17:51 -0400

    Changing the headers for unified file copyright

--- src/provider_vision/media/camera/base_camera.h
+++ src/provider_vision/media/camera/base_camera.h
@@ -1,12 +1,22 @@
-/**
- * \file	base_camera.h
- * \author	Jérémie St-Jules <jeremie.st.jules.prevost@gmail.com>
- * \author	Thibaut Mattio <thibaut.mattio@gmail.com>
- * \date	19/05/2015
- * \copyright	Copyright (c) 2015 SONIA AUV ETS. All rights reserved.
- * Use of this source code is governed by the MIT license that can be
- * found in the LICENSE file.
- */
+/// \author	Pierluc Bédard <pierlucbed@gmail.com>
+/// \author	Jérémie St-Jules Prévôt <jeremie.st.jules.prevost@gmail.com>
+/// \author	Thibaut Mattio <thibaut.mattio@gmail.com>
+/// \copyright Copyright (c) 2015 S.O.N.I.A. All rights reserved.
+/// \section LICENSE
+/// This file is part of S.O.N.I.A. software.
+///
+/// S.O.N.I.A. software is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// S.O.N.I.A. software is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with S.O.N.I.A. software. If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef PROVIDER_VISION_MEDIA_CAMERA_BASE_CAMERA_H_
 #define PROVIDER_VISION_MEDIA_CAMERA_BASE_CAMERA_H_
@@ -25,7 +35,7 @@ namespace provider_vision {
  * Media class.
  * This enables feature and open/close stuff.
  */
-class BaseCamera : public BaseMedia {
+class BaseCamera : public BaseMedia, protected CameraConfiguration {
  public:
   //==========================================================================
   // T Y P E D E F   A N D   E N U M
@@ -92,8 +102,7 @@ class BaseCamera : public BaseMedia {
 
   bool HasArtificialFramerate() const override;
 
-  float GetCameraMsvLum() const;
-  float GetCameraMsvSat() const;
+  void PublishCameraFeatures() const;
 
  protected:
   //==========================================================================
@@ -130,8 +139,6 @@ class BaseCamera : public BaseMedia {
   virtual double GetWhiteBalanceRed() const = 0;
   virtual double GetWhiteBalanceBlue() const = 0;
 
-
-
   void Calibrate(cv::Mat const &img);
 
   float CalculateMSV(const cv::Mat &img, int nbrRegion);
@@ -140,7 +147,7 @@ class BaseCamera : public BaseMedia {
   //==========================================================================
   // P R O T E C T E D   M E M B E R S
 
-  CameraConfiguration config_;
+  ros::Publisher feature_pub_;
 
   FeatureValues current_features_;
 
@@ -152,18 +159,17 @@ class BaseCamera : public BaseMedia {
 
   float msv_lum_, msv_sat_;
 
-  mutable std::mutex msv_acces_;
-
  private:
   //==========================================================================
   // P R I V A T E   M E T H O D S
 
   double UpdatePID(SPid &pid, double error, double position) ATLAS_NOEXCEPT;
+
   cv::Mat CalculateLuminanceHistogram(const cv::Mat &img) const;
   cv::Mat CalculateSaturationHistogram(const cv::Mat &img) const;
 
-
-  friend class CameraParametersListenerTest;
+  //==========================================================================
+  // P R I V A T E   M E M B E R S
 };
 
 //==============================================================================
--- src/provider_vision/media/media_streamer.h
+++ src/provider_vision/media/media_streamer.h
@@ -1,12 +1,22 @@
-/**
- * \file	media_streamer.h
- * \author	Jérémie St-Jules <jeremie.st.jules.prevost@gmail.com>
- * \author	Thibaut Mattio <thibaut.mattio@gmail.com>
- * \date	12/10/2015
- * \copyright	Copyright (c) 2015 SONIA AUV ETS. All rights reserved.
- * Use of this source code is governed by the MIT license that can be
- * found in the LICENSE file.
- */
+/// \author	Pierluc Bédard <pierlucbed@gmail.com>
+/// \author	Jérémie St-Jules Prévôt <jeremie.st.jules.prevost@gmail.com>
+/// \author	Thibaut Mattio <thibaut.mattio@gmail.com>
+/// \copyright Copyright (c) 2015 S.O.N.I.A. All rights reserved.
+/// \section LICENSE
+/// This file is part of S.O.N.I.A. software.
+///
+/// S.O.N.I.A. software is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// S.O.N.I.A. software is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with S.O.N.I.A. software. If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef PROVIDER_VISION_MEDIA_MEDIA_STREAMER_H_
 #define PROVIDER_VISION_MEDIA_MEDIA_STREAMER_H_
@@ -89,12 +99,6 @@ class MediaStreamer : public atlas::Subject<const cv::Mat &>,
    */
   bool IsStreaming() const;
 
-  //To be brought back in the private member 
-  mutable std::mutex image_access_;
-
-  BaseMedia::Ptr media_;
-
-
  protected:
   //==========================================================================
   // P R O T E C T E D   M E T H O D S
@@ -151,10 +155,12 @@ class MediaStreamer : public atlas::Subject<const cv::Mat &>,
   /**
    * Protection of concurrency access between getImage and run.
    */
+  mutable std::mutex image_access_;
 
   /**
    * Active media of the loop
    */
+  BaseMedia::Ptr media_;
 
   /**
    * FrameRate members
@@ -189,8 +195,6 @@ class MediaStreamer : public atlas::Subject<const cv::Mat &>,
   bool is_recording_;
 
   mutable std::mutex list_access_;
-
-  friend class CameraParametersListenerTest;
 };
 
 //==============================================================================
@@ -200,7 +204,7 @@ class MediaStreamer : public atlas::Subject<const cv::Mat &>,
 //
 inline const std::string &MediaStreamer::GetMediaName() const {
   return media_->GetName();
-};
+}
 
 //------------------------------------------------------------------------------
 //
